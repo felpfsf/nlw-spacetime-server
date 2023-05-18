0b9ff2b757ab988e7be0e6269657ddfd32f8fcff
Felipe <feferreira.sf@gmail.com>
2023-05-18 18:08:49 -0300

    feat: fastify jwt import and auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,26 @@
-import cors from '@fastify/cors'
-import Fastify from 'fastify'
-import { memoriesRoutes } from './routes/memories'
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import Fastify from "fastify";
+import { authRoutes } from "./routes/auth";
+import { memoriesRoutes } from "./routes/memories";
 
-const port = (process.env.PORT as unknown as number) || 3333
+const port = (process.env.PORT as unknown as number) || 3333;
 
 async function bootstrap() {
   const server = Fastify({
     logger: true,
-  })
+  });
 
   await server.register(cors, {
     origin: true,
-  })
+  });
 
-  await server.register(memoriesRoutes, { prefix: '/api/memories' })
+  await server.register(jwt, {
+    secret: process.env.SECRET_JWT!,
+  });
+
+  await server.register(authRoutes, { prefix: "/api/auth" });
+  await server.register(memoriesRoutes, { prefix: "/api/memories" });
 
   await server
     .listen({
@@ -21,9 +28,9 @@ async function bootstrap() {
     })
     .then((address) => console.log(`Server listening on ${address}`))
     .catch((error) => {
-      console.error(`Error starting server: ${error}`)
-      process.exit(1)
-    })
+      console.error(`Error starting server: ${error}`);
+      process.exit(1);
+    });
 }
 
-bootstrap()
+bootstrap();
